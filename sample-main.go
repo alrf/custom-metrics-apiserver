7053ffdf2fe496735d8f343c6d24906de93a09bb
Steve McQuaid <steve@stevemcquaid.com>
2018-04-11 17:12:15 -0400

    First Commit

--- sample-main.go
+++ sample-main.go
@@ -21,7 +21,7 @@ import (
 	"os"
 	"runtime"
 
-	"github.com/kubernetes-incubator/custom-metrics-apiserver/pkg/sample-cmd/server"
+	"github.com/stevemcquaid/custom-metrics-apiserver/pkg/sample-cmd/server"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/apiserver/pkg/util/logs"
 )
